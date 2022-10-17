85694c3c27ce1ca3d1a27227002ce6516a48e492
HiBixby <201910789@sangmyung.kr>
2022-10-17 18:08:56 +0900

    🚸 Add "ex)" text in placeholder of input

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
         class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
         v-model.lazy="name"
         required
-        placeholder="홍길동"
+        placeholder="ex) 홍길동"
       />
       <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
         이름을 입력해주세요.
@@ -36,7 +36,7 @@
         class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
         autocomplete="off"
         v-model.lazy="content"
-        placeholder="안녕하세요!"
+        placeholder="ex) 안녕하세요!"
         required
       />
 
